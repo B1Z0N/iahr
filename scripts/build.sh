aeee86d30f22801f2877bea4578964b4f4b076ff
kraftwerk28 <kefirchik3@gmail.com>
2020-01-27 23:00:17 +0200

    chore: scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,9 @@
 docker-compose \
   -f docker-compose.yml \
+  -f docker-compose.deploy.yml \
   build
 
 docker-compose \
   -f docker-compose.yml \
+  -f docker-compose.deploy.yml \
   push
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,10 @@
 export DOCKER_HOST="ssh://kraftwerk28.pp.ua"
-docker-compose \
-  -f docker-compose.yml \
-  pull
 
 docker-compose \
   -f docker-compose.yml \
+  -f docker-compose.deploy.yml \
+  pull
+
+docker-compose \
+  -f docker-compose.deploy.yml \
   up -d
