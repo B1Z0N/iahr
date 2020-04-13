1d1f5bbbeb571de887f0b47d7dbdd514d1c17d1f
B1Z0N <kolausf@gmail.com>
2020-04-14 01:23:11 +0300

    Completed tokenizer

--- src/utils.py
+++ src/utils.py
@@ -10,25 +10,56 @@ class Query:
     # matches all [some text] including [some \[text\]] 
     DELIMITER_RE = re.compile(r'\[([^\]\[\\]*(?:\\.[^\]\[\\]*)*)]')
     
+    class SyntaxError(Exception):
+        """ 
+            Plug exception to tell that some input is faulty 
+        """
+        pass
 
-    def __init__(command: str, args: Union[Iterable[str], Query]]):
+    def __init__(self, command: str, args):
         self.command = command
-        self.args = args    
+        self.args = args # Could be: List[str], Query, None
     
     @classmethod
-    def parse(cls, qstr, final=False) -> Union[Query, None]: 
-        qstr = qstr.strip()
+    def from_str(cls, qstr):
+        try:
+            res = cls.__parse(qstr, final)
+        except ValueError as e:
+            raise SyntaxError(*e.args)
+
+        if res is None:
+            raise SyntaxError()
+        return res
+ 
+    @classmethod
+    def __parse(cls, qstr): 
+        qstr = qstr.strip() + ' '
         if qstr.startswith('.'):
             command = qstr[1:qstr.index(' ')]
-            qstr = qstr[qstr.index('_'):].strip()
+            qstr = qstr[qstr.index(' '):].strip()
  
-            subcommand = cls.parse(qstr)
-            if subcommand is None:
-                is_single_arg = not qstr.startswith(self.LEFT_DELIMITER) or \
-                                not qstr.endswith(self.RIGHT_DELIMITER)
-                args = [qstr] if is_single_arg else re.findall(DELIMITER_RE, qstr)
+            subcommand = cls.__parse(qstr)
+            if subcommand is None and qstr:
+                is_single_arg = not qstr.startswith(cls.LEFT_DELIMITER) or \
+                                not qstr.endswith(cls.RIGHT_DELIMITER)
+                args = [qstr] if is_single_arg else re.findall(cls.DELIMITER_RE, qstr)
             else:
                 args = subcommand
             
             return cls(command, args)
+
+def test_query(qstr):
+    q = Query.from_str(qstr)
+    def print_query(q):
+        print("Command:", q.command)
+        if type(q.args) == type([]):
+            print("Args:", q.args)
+        elif q.args is None:
+            pass
+        else: 
+            print_query(q.args)
+        
+    print_query(q)
+
+test_query(input())
 
