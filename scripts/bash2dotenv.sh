e36db09d5da6b76b585d2ab82a885e4d8a61cc58
B1Z0N <kolausf@gmail.com>
2020-05-03 14:16:03 +0300

    Removed unnecessary scripts

--- scripts/bash2dotenv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -z "$1" ] || [ "$1" = "--help" ]; then
-    printf "You should pass the filename to transform bash script to dot env file\n"
-    exit
-fi
-
-sed -r '/^\s*$/d' "$1" | sed '/^#/ d' | sed -e 's/^export //'
-
